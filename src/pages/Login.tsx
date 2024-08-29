c581f2ac75ef88a92f8b119467d71561d7a174ec
mdferdousalam <mdferdousalam1989@yahoo.com>
2024-08-29 23:33:30 +0600

    s

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,47 +1,25 @@
 import {
-   Button,
-  Center,
-  Divider,
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormHelperText,
+  Heading,
   Input,
   InputGroup,
+  InputLeftElement,
   InputRightElement,
+  Link,
+  Stack,
   useToast,
-  VStack,
 } from "@chakra-ui/react";
 import * as React from "react";
+import { FaLock, FaUserAlt } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-import { ProtectedComponent } from "../components/ui/auth/ProtectedComponent";
-import { setCredentials } from "../redux/features/auth/authSlice";
 import { useAppDispatch } from "../hooks/hook";
 import { LoginRequest, useLoginMutation } from "../redux/features/auth/auth";
-
-function PasswordInput({
-  name,
-  onChange,
-}: {
-  name: string;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-}) {
-  const [show, setShow] = React.useState(false);
-  const handleClick = () => setShow(!show);
-
-  return (
-    <InputGroup size="md">
-      <Input
-        pr="4.5rem"
-        type={show ? "text" : "password"}
-        placeholder="Enter password"
-        name={name}
-        onChange={onChange}
-      />
-      <InputRightElement width="4.5rem">
-        <Button h="1.75rem" size="sm" onClick={handleClick}>
-          {show ? "Hide" : "Show"}
-        </Button>
-      </InputRightElement>
-    </InputGroup>
-  );
-}
+import { setCredentials } from "../redux/features/auth/authSlice";
 
 export default function LoginPage() {
   const dispatch = useAppDispatch();
@@ -53,6 +31,8 @@ export default function LoginPage() {
     password: "",
   });
 
+  const [showPassword, setShowPassword] = React.useState(false);
+
   const [login, { isLoading }] = useLoginMutation();
 
   const handleChange = ({
@@ -60,47 +40,113 @@ export default function LoginPage() {
   }: React.ChangeEvent<HTMLInputElement>) =>
     setFormState((prev) => ({ ...prev, [name]: value }));
 
-  return (
-    <Center h="500px">
-      <VStack spacing="4">
-        <InputGroup>
-          <Input
-            onChange={handleChange}
-            name="username"
-            type="text"
-            placeholder="Email"
-          />
-        </InputGroup>
+  const handleShowClick = () => setShowPassword(!showPassword);
 
-        <InputGroup>
-          <PasswordInput onChange={handleChange} name="password" />
-        </InputGroup>
-        <Button
-          // isFullWidth
-          onClick={async () => {
-            try {
-              const userData = { ...formState, role: "user" };
-              const user = await login(userData).unwrap();
-              dispatch(setCredentials(user));
-              navigate("/");
-            } catch (err) {
-              console.log(err);
-              toast({
-                status: "error",
-                title: "Error",
-                description: "Oh no, there was an error!",
-                isClosable: true,
-              });
-            }
-          }}
-          colorScheme="green"
-          isLoading={isLoading}
-        >
-          Login
-        </Button>
-        <Divider />
-        <ProtectedComponent />
-      </VStack>
-    </Center>
+  return (
+    <Flex
+      flexDirection="column"
+      width="100wh"
+      height="100vh"
+      backgroundColor="gray.200"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack
+        flexDir="column"
+        mb="2"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Avatar bg="teal.500" />
+        <Heading color="teal.400">Welcome</Heading>
+        <Box minW={{ base: "90%", md: "468px" }}>
+          <form>
+            <Stack
+              spacing={4}
+              p="1rem"
+              backgroundColor="whiteAlpha.900"
+              boxShadow="md"
+              borderRadius="lg"
+            >
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<FaUserAlt color="gray.300" />}
+                  />
+                  <Input
+                    type="email"
+                    placeholder="email address"
+                    onChange={handleChange}
+                    name="email"
+                    bg="white"
+                    borderColor="gray.300"
+                    _focus={{ borderColor: "teal.500" }}
+                  />
+                </InputGroup>
+              </FormControl>
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    color="gray.300"
+                    children={<FaLock color="gray.300" />}
+                  />
+                  <Input
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password"
+                    onChange={handleChange}
+                    name="password"
+                    bg="white"
+                    borderColor="gray.300"
+                    _focus={{ borderColor: "teal.500" }}
+                  />
+                  <InputRightElement width="4.5rem">
+                    <Button h="1.75rem" size="sm" onClick={handleShowClick}>
+                      {showPassword ? "Hide" : "Show"}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+                <FormHelperText textAlign="right">
+                  <Link color="teal.500">forgot password?</Link>
+                </FormHelperText>
+              </FormControl>
+              <Button
+                borderRadius={0}
+                type="submit"
+                variant="solid"
+                colorScheme="teal"
+                width="full"
+                isLoading={isLoading}
+                onClick={async () => {
+                  try {
+                    const userData = { ...formState, role: "user" };
+                    const user = await login(userData).unwrap();
+                    dispatch(setCredentials(user));
+                    navigate("/");
+                  } catch (err) {
+                    console.log(err);
+                    toast({
+                      status: "error",
+                      title: "Error",
+                      description: "Oh no, there was an error!",
+                      isClosable: true,
+                    });
+                  }
+                }}
+              >
+                Login
+              </Button>
+            </Stack>
+          </form>
+        </Box>
+      </Stack>
+      <Box>
+        New to us?{" "}
+        <Link color="teal.500" href="#">
+          Sign Up
+        </Link>
+      </Box>
+    </Flex>
   );
 }
